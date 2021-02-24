6575f7a5cffca1bde3a8bde8efa3ee0f50f70e86
saturdayquant <markusskagemo@gmail.com>
2021-02-24 15:01:49 +0100

    koble koble

---
+++ visualization/.ipynb_checkpoints/visualization-checkpoint.py
@@ -0,0 +1,29 @@
+import numpy as np
+import plotly.graph_objects as go
+
+
+def visualize_mask(mask, cube, ndgrid_idxs=slice(0, 1, 50j)):
+    filled = np.add(cube, mask)
+    x, y, z = np.mgrid[ndgrid_idxs, ndgrid_idxs, ndgrid_idxs]
+
+    fig = go.Figure(data=go.Volume(
+        x=x.flatten(),
+        y=y.flatten(),
+        z=z.flatten(),
+        #value=filled.flatten(),
+        value=mask.flatten(),
+        isomin=0.1,
+        isomax=0.8,
+        opacity=0.1,       # needs to be small to see through all surfaces
+        surface_count=17,  # needs to be a large number for good volume rendering
+    ))
+    ax_layout = {
+        'visible': False, 
+        'showticklabels': False, 
+        'showgrid': False, 
+        'zeroline': False
+    }
+    fig.update_layout(scene={
+        'xaxis': ax_layout, 'yaxis': ax_layout, 'zaxis': ax_layout
+    })
+    fig.show()
--- visualization/visualization.py
+++ visualization/visualization.py
@@ -2,29 +2,28 @@ import numpy as np
 import plotly.graph_objects as go
 
 
-cube = np.ones(shape=(50, 50, 50))
-cube = np.multiply(cube, 0.1)
-mask = np.copy(cube)
-mask = np.multiply(mask, 0)
-mask[18:20, 19:21, 12:15] = 1
-filled = np.add(cube, mask)
+def visualize_mask(mask, cube, ndgrid_idxs=slice(0, 1, 50j)):
+    filled = np.add(cube, mask)
+    x, y, z = np.mgrid[ndgrid_idxs, ndgrid_idxs, ndgrid_idxs]
 
-x, y, z = np.mgrid[0:1:50j, 0:1:50j, 0:1:50j]
-
-fig = go.Figure(data=go.Volume(
-    x=x.flatten(),
-    y=y.flatten(),
-    z=z.flatten(),
-    value=filled.flatten(),
-    isomin=0.1,
-    isomax=0.8,
-    opacity=0.1,       # needs to be small to see through all surfaces
-    surface_count=17,  # needs to be a large number for good volume rendering
+    fig = go.Figure(data=go.Volume(
+        x=x.flatten(),
+        y=y.flatten(),
+        z=z.flatten(),
+        #value=filled.flatten(),
+        value=mask.flatten(),
+        isomin=0.1,
+        isomax=0.8,
+        opacity=0.1,       # needs to be small to see through all surfaces
+        surface_count=17,  # needs to be a large number for good volume rendering
     ))
-
-fig.update_layout(
-    scene={'xaxis': {'visible': False, 'showticklabels': False, 'showgrid': False, 'zeroline': False},
-           'yaxis': {'visible': False, 'showticklabels': False, 'showgrid': False, 'zeroline': False},
-           'zaxis': {'visible': False, 'showticklabels': False, 'showgrid': False, 'zeroline': False}})
-
-fig.show()
+    ax_layout = {
+        'visible': False, 
+        'showticklabels': False, 
+        'showgrid': False, 
+        'zeroline': False
+    }
+    fig.update_layout(scene={
+        'xaxis': ax_layout, 'yaxis': ax_layout, 'zaxis': ax_layout
+    })
+    fig.show()
