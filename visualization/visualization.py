0009b14a2d41b2d97da4c1d1641d200c693de3b6
saturdayquant <markusskagemo@gmail.com>
2021-03-24 14:40:31 +0100

    før påske

--- visualization/visualization.py
+++ visualization/visualization.py
@@ -3,19 +3,9 @@ import plotly.graph_objects as go
 
 
 def visualize_mask(filled, ndgrid_idxs=slice(0, 1, 50j)):
-    x, y, z = np.mgrid[ndgrid_idxs, ndgrid_idxs, ndgrid_idxs]
-
-    fig = go.Figure(data=go.Volume(
-        x=x.flatten(),
-        y=y.flatten(),
-        z=z.flatten(),
-        value=filled.flatten(),
-        isomin=0.1,
-        isomax=0.8,
-        opacity=0.1,       # needs to be small to see through all surfaces
-        surface_count=17,  # needs to be a large number for good volume rendering
-    ))
-
+    vol = create_volume(filled, ndgrid_idxs)
+    
+    fig = go.Figure(data=vol)
     ax_layout = {
         'visible': False, 
         'showticklabels': False, 
@@ -28,3 +18,18 @@ def visualize_mask(filled, ndgrid_idxs=slice(0, 1, 50j)):
     })
     """
     return fig
+
+
+def create_volume(data, ndgrid_idxs, colorscale='Viridis', opacity=0.1, n_surface=17):
+    x, y, z = np.mgrid[ndgrid_idxs, ndgrid_idxs, ndgrid_idxs]
+    return go.Volume(
+        x=x.flatten(),
+        y=y.flatten(),
+        z=z.flatten(),
+        value=data.flatten(),
+        colorscale=colorscale,
+        isomin=0.1,
+        isomax=0.8,
+        opacity=opacity,
+        surface_count=n_surface,  # needs to be a large number for good volume rendering
+    )
